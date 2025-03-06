21771a52de802f1cd8f8258b123228c2ce358033
Nam <namada871@gmail.com>
2025-03-06 22:25:37 +0300

    makes the admin login page and dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,27 @@ import AdminLogin from "./layout/AdminLogin";
 import UserSignup from "./layout/UserSignup";
 import UserLogin from "./layout/UserLogin";
 import Dashboard from "./layout/Dashboard";
+import AdminDashboard from "./layout/AdminDashboard";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
+    <div className="w-screen h-screen">
+    <div>
+    <Navbar />
+    </div>
+    
     
       <Routes>
         <Route path="/" element={<UserLogin />} />
         <Route path="/login" element={<UserLogin />} />
         <Route path="/signup" element={<UserSignup />} />
-        <Route path="/admin" element={<AdminLogin />} />
         <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/AdminDashboard" element={<AdminDashboard />} />
       </Routes>
+    </div>
+    
     
   );
 };
